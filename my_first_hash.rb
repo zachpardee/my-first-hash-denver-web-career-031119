933f741dfd57e68988c5939f7fd0be222beb8b56
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-10 19:29:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  your_hash  = 
+  your_hash  = {a_key => "a_value"}
 end
 
 
